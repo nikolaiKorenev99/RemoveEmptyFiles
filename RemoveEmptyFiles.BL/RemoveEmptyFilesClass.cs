a38ae553b90fc108894fd51a23e61237ec004a27
nikolajkrut <sergeykrasawa@gmail.com>
2019-08-02 16:49:03 +0300

    Добавьте файлы проекта.

---
+++ RemoveEmptyFiles.BL/RemoveEmptyFilesClass.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveEmptyFiles.BL
+{
+    public class RemoveEmptyFilesClass
+    {
+        public int RemoveFiles(ref FileInfo[] fAr)
+        {
+            int count = 0;
+            foreach (var item in fAr)
+            {
+                item.Delete();
+                count++;
+            }
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+
+            }
+            return count;
+        }
+        public async Task<int> RemoveFilesAsync(FileInfo[] fAr)
+        {
+            return await Task<int>.Run(() =>
+            {
+                return RemoveFiles(ref fAr);
+            });
+        }
+        public string AppendFindFiles(ref FileInfo[] fAr)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("Find " + fAr?.Length + " files");
+            result.Append(Environment.NewLine);
+            foreach (var f in fAr)
+            {
+                if (f.Length == 0)
+                {
+                    result.Append(f.FullName);
+                    result.Append(Environment.NewLine);
+                }
+            }
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+
+            }
+            return result.ToString();
+        }
+        public async Task<string> AppendFindFilesAsync(FileInfo[] fAr)
+        {
+            return await Task<string>.Run(() =>
+            {
+            return AppendFindFiles(ref fAr);
+        });
+
+        }
+}
+}
---
+++ RemoveEmptyFiles/Form1.cs
@@ -0,0 +1,76 @@
+﻿using RemoveEmptyFiles.BL;
+using System;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RemoveEmptyFiles
+{
+    public partial class Form1 : Form
+    {
+        FileInfo[] fAr;
+        RemoveEmptyFilesClass removeEmptyFilesClass;
+        public Form1()
+        {
+            InitializeComponent();
+            removeEmptyFilesClass = new RemoveEmptyFilesClass();
+            this.MaximizeBox = false;
+            this.Text = "Remove empty files";
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            button1.Text = "Choose directory";
+            button1.Font = textBox1.Font = button2.Font = new Font("Arial", 15);
+            button2.Text = "Remove";
+            button2.Enabled = false;
+
+            textBox1.ReadOnly = true;
+            textBox1.Multiline = true;
+            textBox1.ScrollBars = ScrollBars.Both;
+
+            button1.Click += SelectDirectoryClick;
+            button2.Click += RemoveFilesClick;
+
+
+        }
+        private async void RemoveFilesClick(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            textBox1.Text = "Please wait...";
+            int count = await removeEmptyFilesClass?.RemoveFilesAsync(fAr);
+            textBox1.Text = count.ToString() + " files deleted";
+        }
+
+        private async void SelectDirectoryClick(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    textBox1.Text = fbd.SelectedPath.ToString();
+                    await Task.Run(() =>
+                    {
+                        DirectoryInfo di = new DirectoryInfo(fbd.SelectedPath);
+                        fAr = null;
+                        fAr = di.GetFiles()
+                                           .Where(x => x.Length == 0).ToArray();
+                    });
+                }
+                else
+                {
+                    textBox1.Text = "Error" + fbd.SelectedPath;
+                }
+            }
+            textBox1.AppendText(Environment.NewLine);
+            textBox1.AppendText(await removeEmptyFilesClass?.AppendFindFilesAsync(fAr));
+            textBox1.SelectionStart = 0;
+            textBox1.ScrollToCaret();
+            if (fAr.Length > 0)
+            {
+                button2.Enabled = true;
+            }
+        }
+    }
+}
